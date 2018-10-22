6f04fe623855de79a53b1ed78669ebf1dee22904
Carmine Sirignano <carmines@microsoft.com>
2018-10-21 21:24:57 -0700

    adding license header

--- CameraCapture/CameraCapture/Source/Shared/Media.Capture.MrcAudioEffect.h
+++ CameraCapture/CameraCapture/Source/Shared/Media.Capture.MrcAudioEffect.h
@@ -1,4 +1,7 @@
-﻿#pragma once
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
 
 #include "Media/Capture/MrcAudioEffect.g.h"
 #include <winrt/Windows.Foundation.Collections.h>
